5d226a9f35fc8f3c6aa6525649d2a794e6a8f6c4
unknown <daipengpeng@mail.clipslightai.top>
2024-03-26 17:57:36 +0800

    socket 部分

--- src/cmd.rs
+++ src/cmd.rs
@@ -1,7 +1,18 @@
 use local_ip_address;
 #[tauri::command]
 pub fn get_ipaddr() -> String {
-    println!("-------------");
     let interfaces = local_ip_address::list_afinet_netifas().unwrap();
-    interfaces.iter().map(|(_, ip)| ip.to_string()).collect::<String>()
+    let mut s = interfaces.iter().filter(|(_, ip)| ip.to_string().len() > 10 && ip.to_string().len() <= 16).map(|(_, ip)| ip.to_string() + ",").collect::<String>();
+    s.pop();
+    s
+}
+#[tauri::command]
+pub fn open_socket(addr: String, port: String, multaddr: String) -> () {
+    let socket = std::net::UdpSocket::bind(addr).unwrap();
+    socket.set_broadcast(true).unwrap();
+    socket.set_multicast_loop_v4(false).unwrap();
+    if !multaddr.is_empty() {
+        let multaddr = multaddr.split('.').map(|s| s.parse().unwrap()).collect::<Vec<u8>>();
+        socket.join_multicast_v4(&std::net::Ipv4Addr::new(multaddr[0], multaddr[1], multaddr[2], multaddr[3]), &std::net::Ipv4Addr::new(0, 0, 0, 0)).unwrap();
+    }
 }
--- ui/index.js
+++ ui/index.js
@@ -1,19 +1,24 @@
+function inputValueChanged() {
+    const input = document.querySelector('input')
+    input.value = input.value.replace(/\D/g, '')
+}
 document.addEventListener('DOMContentLoaded', () => {
+    const form = document.querySelector('form')
+    form.addEventListener('submit', event => {
+        event.preventDefault()
+    })
+    form.querySelector('button').addEventListener('click', () => invoke())
+    const input = document.querySelector('input')
+    input.addEventListener('input', () => inputValueChanged())
+    input.addEventListener('afterpaste', () => inputValueChanged())
     const { invoke } = window.__TAURI__.tauri
     invoke('get_ipaddr').then((addrs) => {
         const select = document.querySelector('select')
-        /*
         const strList = addrs.split(',')
         strList.forEach((addr, _) => {
             const option = document.createElement('option')
             option.innerText = addr
             select.appendChild(option)
         })
-        */
-        const option = document.createElement('option')
-        console.log("------------")
-        console.log(addrs)
-        option.innerText = addrs
-        select.appendChild(option)
     })
 })
